cd0868d055b02c46393b61a35001ede836f90774
Chintamani Prasad <cmanix07@users.noreply.github.com>
2020-02-01 10:44:09 +0900

    Junit5 Basic

---
+++ junit5-basics/src/main/java/com/rakuten/MathUtils.java
@@ -0,0 +1,10 @@
+package com.rakuten;
+
+public class MathUtils {
+
+	int a;
+	int b;
+	public int calculateSum(int a, int b) {
+		return a+b;
+	}
+}
---
+++ junit5-basics/src/test/java/com/rakuten/MathUtilsTest.java
@@ -0,0 +1,14 @@
+package com.rakuten;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void testCalculateSum() {
+		fail("Not yet implemented");
+	}
+
+}
